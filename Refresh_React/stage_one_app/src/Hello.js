d94ad2b4b436d51cc93b0a26a2342bb84b7a7201
Benchov <viktor.mihalyi@yahoo.com>
2023-05-23 09:06:00 +0200

    feat(Hello.js): Initial add of new component

---
+++ Refresh_React/stage_one_app/src/Hello.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import './hello.css'
+
+
+class Hello extends Component {
+    render () {
+        return (
+            <div className='f1 tc'>
+                <h1>Helloka Pottom</h1>    
+                <p>Aranyosan alszol!</p>        
+            </div>
+        )
+    }
+}
+
+export default Hello
+
