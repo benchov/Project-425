f5de81ca1cdf2add6db16e5dd937f4d1298086c9
Benchov <viktor.mihalyi@yahoo.com>
2023-05-24 11:48:11 +0200

    feat(App.js): Reorganize  app structure

--- Refresh_React/stage_one_app/src/index.js
+++ Refresh_React/stage_one_app/src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-import CardList from './CardList.js'
-import {robots} from './robots.js';
-
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots} />
+    <App />
   </React.StrictMode>
 );
 
