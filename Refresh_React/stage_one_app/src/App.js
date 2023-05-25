79ffb74f04df3a95d9e3a828411e38d3f9e9c0d5
Benchov <viktor.mihalyi@yahoo.com>
2023-05-25 18:09:41 +0200

    feat(App.js): Add filter functionality

--- Refresh_React/stage_one_app/src/App.js
+++ Refresh_React/stage_one_app/src/App.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
+
+
 const App = () => {
+    const [searchValue, setSearchValue] = useState('');
+    const [filteredRobots, setFilteredRobots] = useState(robots)
+
+    const handleFilter = () => {
+        searchValue.length > 1 ? setFilteredRobots(robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(searchValue.toLowerCase())
+        })) : setFilteredRobots(robots)
+    }
+    const handleSearch = (e) => {
+        setSearchValue(e)
+        handleFilter()
+    }
+
     return (
         <div>
             <h1>RobotFriends</h1>
-            <SearchBox />
-            <CardList robots={robots} />
+            <SearchBox searchOnChange={(e) => handleSearch(e.target.value)} />
+            <CardList robots={filteredRobots}/>
         </div>
     )
-
 }
 
 export default App;
