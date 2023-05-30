a0b82c72f94f4a3107fc1c1440f4793f42aab649
Benchov <viktor.mihalyi@yahoo.com>
2023-05-30 13:23:02 +0200

    feat(_app.js): add footer to the project

--- Learn_Next_js/discover-coffee-basic/pages/_app.js
+++ Learn_Next_js/discover-coffee-basic/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <p>this is created by benchov_the_zen</p>
+    </div>
+  )
 }
