ac51a054e367ebdbaadf635b341c3b0b5c483bdb
Benchov <viktor.mihalyi@yahoo.com>
2023-05-30 13:22:33 +0200

    feat(hello.js): add dedicated style

--- Learn_Next_js/discover-coffee-basic/pages/hello.js
+++ Learn_Next_js/discover-coffee-basic/pages/hello.js
@@ -1,5 +1,11 @@
+import styles from '../styles/hello.module.css'
+
 const Hello = () => {
-    return <h1>HELLO</h1>
+    return (
+        <div className={styles.container}>
+            <h1>Hello with style</h1>
+        </div>
+    )
 }
 
 export default Hello;
