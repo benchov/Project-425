099f398d75981b6c88e994d62a86ff364460e19f
Benchov <viktor.mihalyi@yahoo.com>
2023-06-07 22:50:01 +0200

    feat(coffee-shop):Extend component with Link

--- Learn_Next_js/discover-coffee-basic/pages/coffee-shop/[id].js
+++ Learn_Next_js/discover-coffee-basic/pages/coffee-shop/[id].js
@@ -1,8 +1,17 @@
 import { useRouter } from "next/router";
+import Link from 'next/link';
+
 
 const CoffeeShop = () => {
     const router = useRouter();
-    return <div>{router.query.id}</div>
+    return (
+        <div>
+            <div>{router.query.id}</div>
+            <Link href="http://localhost:3000/">
+            <p>Back to home</p>
+             </Link>
+        </div>
+    )
 }
 
 export default CoffeeShop;
