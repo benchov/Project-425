b0915da4e695f240f94809a277043d88f9c2e12e
Benchov <viktor.mihalyi@yahoo.com>
2023-06-09 13:30:27 +0200

    fix(index.js): remove unnecessary code

--- Learn_Next_js/discover-coffee-basic/pages/index.js
+++ Learn_Next_js/discover-coffee-basic/pages/index.js
@@ -1,10 +1,7 @@
 import Image from 'next/image'
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import styles from "../styles/index.module.css"
 import Banner from '../components/banner';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
