a6fa192e9d0eefc6d96ee91df7b30dca1025133f
Benchov <viktor.mihalyi@yahoo.com>
2023-06-09 13:08:40 +0200

    feat(root.js): Missed exercise task
    This change will dynamicly update the title

--- Learn_Next_js/discover-coffee-basic/pages/[root].js
+++ Learn_Next_js/discover-coffee-basic/pages/[root].js
@@ -1,11 +1,15 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
 
 const DynamicRootComponent = () => {
     const router = useRouter()
     return (
     <div>
-        <p>{router.query.root}</p>
+        <Head>
+            <title>{router.query.root}</title>
+        </Head>
+        
         <p>this is a dynamic route</p>
         <Link href="http://localhost:3000/courses/next.js">
             <p>Go to next.js page</p>
