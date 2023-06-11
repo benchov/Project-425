20bc1d6b9af4481cc359d362067b6406a626be87
Benchov <viktor.mihalyi@yahoo.com>
2023-06-11 22:16:01 +0200

    style(_document.js): Add new font family

--- Learn_Next_js/discover-coffee-basic/pages/_document.js
+++ Learn_Next_js/discover-coffee-basic/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preload" href='/fonts/IBMPlexSans-Bold.ttf' as="fonts" crossOrigin='anonymus'/>
+        <link rel="preload" href='/fonts/IBMPlexSans-Regular.ttf' as="fonts" crossOrigin='anonymus'/>
+        <link rel="preload" href='/fonts/IBMPlexSans-LightItalic.ttf' as="fonts" crossOrigin='anonymus'/>
+      </Head>
       <body>
         <Main />
         <NextScript />
