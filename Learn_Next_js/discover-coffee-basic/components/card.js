9716530dbbd083ab665aa025ccedae97246d5291
Benchov <viktor.mihalyi@yahoo.com>
2023-06-13 10:56:06 +0200

    style(card.js): Add glass effect and style to card component

--- Learn_Next_js/discover-coffee-basic/components/card.js
+++ Learn_Next_js/discover-coffee-basic/components/card.js
@@ -1,15 +1,20 @@
 import styles from '../styles/card.module.css';
 import Image from 'next/image'
 import Link from 'next/link'
+import cls from 'classnames'
 
 const Card = (props) => {
     return (
-        <Link href={props.url}>
- 
-            <h2 className={styles.title}>{props.cardTitle}</h2>
-            <Image src={props.imgUrl} width={260} height={160}/> 
-
-    </Link>
+        <Link className={styles.cardLink} href={props.url}>
+            <div className={cls("glass",styles.container)}>
+                <div className={styles.cardHeaderWrapper}>
+                    <h2 className={styles.cardHeader}>{props.cardTitle}</h2>
+                </div>
+                <div className={styles.cardImageWrapper}>
+                    <Image src={props.imgUrl} width={260} height={160}/> 
+                </div>
+            </div>
+        </Link>
     )
 }
 
