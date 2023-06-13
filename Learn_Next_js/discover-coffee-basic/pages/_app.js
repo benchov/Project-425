f44774028e8bd17b255d1da91b115477a46d6da7
Benchov <viktor.mihalyi@yahoo.com>
2023-06-13 14:43:48 +0200

    refacator(_app.js): remove unused codes

--- Learn_Next_js/discover-coffee-basic/pages/_app.js
+++ Learn_Next_js/discover-coffee-basic/pages/_app.js
@@ -4,7 +4,6 @@ export default function App({ Component, pageProps }) {
   return (
     <div>
       <Component {...pageProps} />
-      <p>this is created by benchov_the_zen</p>
     </div>
   )
 }
