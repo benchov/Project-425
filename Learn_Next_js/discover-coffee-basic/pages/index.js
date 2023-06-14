b372ac0d67ba42607d3433d38709a6e05d924669
Benchov <viktor.mihalyi@yahoo.com>
2023-06-14 14:05:06 +0200

    feat(index.js): Implement static site generation

--- Learn_Next_js/discover-coffee-basic/pages/index.js
+++ Learn_Next_js/discover-coffee-basic/pages/index.js
@@ -3,11 +3,35 @@ import Head from 'next/head'
 import styles from "../styles/index.module.css"
 import Banner from '../components/banner';
 import Card from '../components/card'
+import coffeeStores from '../data/coffee-stores.json'
 
-export default function Home() {
+export async function getStaticProps (context){
+  return {
+    props: {
+      coffeeStores,
+    }
+  }
+}
+export default function Home(props) {
 
   const handleBannerOnClick = () => {
     console.log('Ossu')
+  }
+
+  const showStores = () => {
+    return props.coffeeStores.map( (coffeeStore) => {
+      return (
+        <Card 
+          key={coffeeStore.id}
+          cardTitle={coffeeStore.name}
+          imgUrl={coffeeStore.imgUrl}
+          url={`/coffee-shop/${coffeeStore.id}`}
+          className={styles.card}
+        />
+      )
+    }
+
+    )
   }
   return (
     <div className={styles.container}>
@@ -19,12 +43,7 @@ export default function Home() {
         <div className={styles.heroImage}>
           <Image src='/static/hero-image.png' width={700} height={400}/>
           <div className={styles.cardLayout}> 
-            <Card 
-              cardTitle="Test Coffee Shop"
-              imgUrl='/static/c1.jpeg'
-              url='/coffee-shop/test1'
-              className={styles.card}
-            />
+            {showStores()}
           </div>
         </div>
       </main>
