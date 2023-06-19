c579fb41a593356f803149e946f499c2f02393f5
Benchov <viktor.mihalyi@yahoo.com>
2023-06-19 07:53:57 +0200

    feat(): Reperesent more informations

--- Learn_Next_js/discover-coffee-basic/pages/coffee-shop/[id].js
+++ Learn_Next_js/discover-coffee-basic/pages/coffee-shop/[id].js
@@ -1,7 +1,11 @@
 import { useRouter } from "next/router";
 import Link from 'next/link';
 import Head from 'next/head';
+import Image from 'next/image';
+
 import coffeStoreData from "../../data/coffee-stores.json"
+import styles from '../../styles/coffe-store.module.css'
+import cls from 'classnames';
 
 export function getStaticProps(staticProps) {
     const params = staticProps.params;
@@ -29,23 +33,50 @@ export function getStaticPaths() {
 }
 const CoffeeShop = (props) => {
     const router = useRouter();
-    const {address, name, neighbourhood} = props.coffeeStore
+    const {address, name, neighbourhood, imgUrl} = props.coffeeStore
+
+    const handleUpVote = () => {
+        console.log("handle up vote pushed")
+    }
     if(router.isFallback) {
         return <div>Loading...</div>
     }
 
     return (
-        <div>
+        <div className={styles.layout}>
             <Head>
                 <title>{name}</title>
             </Head>
-            <Link href="http://localhost:3000/">
-                <p>Back to home</p>
-            </Link>
-            <p>{address}</p>
-            <p>{name}</p>
-            <p>{neighbourhood}</p>
-
+            <div className={styles.container}>
+                <div className={styles.col1}>
+                    <div className={styles.backToHomeLink}>
+                        <Link href="http://localhost:3000/">
+                            <p>Back to home</p>
+                        </Link>
+                    </div>
+                    <div className={styles.nameWrapper}>
+                        <h1 className={styles.name}>{name}</h1>
+                    </div>
+                    <Image src={imgUrl} width={600} height={360} className={styles.storeImage} alt={name}/>
+                </div>
+                <div className={cls("glass",styles.col2)}>
+                    <div className={styles.iconWrapper}>
+                        <Image src="/static/drop.svg" width={24} height={24} />
+                        <p className={styles.text}>{address}</p>
+                    </div>
+                    <div className={styles.iconWrapper}>
+                        <Image src="/static/navi.svg" width={24} height={24} />
+                        <p className={styles.text}>{neighbourhood}</p>
+                    </div>
+                    <div className={styles.iconWrapper}>
+                        <Image src="/static/like.svg" width={24} height={24} />
+                        <p className={styles.text}>1</p>
+                    </div>
+                    <button className={styles.upvoteButton} onClick={handleUpVote}>Handle up Vote</button>
+                </div>
+            </div>
+       
+ 
         </div>
     )
 }
